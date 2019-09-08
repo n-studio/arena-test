370d83d71042e47eeecae3f2dcfe12581acb9c58
Matthew Nguyen <contact@n-studio.fr>
2019-09-08 17:39:47 +0200

    Add decoration

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,11 @@
 Rails.application.routes.draw do
   root to: 'fights#index'
-  resources :fights, except: :destroy do
+  resources :fights do
     resources :fight_steps, path: "steps", as: "steps", only: :show
   end
 
   namespace :admin do
     root to: 'fighters#index'
-    resources :fighters
+    resources :fighters, except: :show
   end
 end
