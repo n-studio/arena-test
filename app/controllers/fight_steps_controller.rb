7413affd22a10b4ca68ba0c981f1e2d404cea955
Matthew Nguyen <contact@n-studio.fr>
2019-09-08 18:13:05 +0200

    Add css to steps

--- app/controllers/fight_steps_controller.rb
+++ app/controllers/fight_steps_controller.rb
@@ -3,14 +3,8 @@ class FightStepsController < ApplicationController
     @fight = Fight.includes(:fight_steps, :fight_attendances, :fighters).find(params[:fight_id])
     @fighters = @fight.fighters
     steps = @fight.fight_steps
-    @step = steps[params[:id].to_i - 1]
-    @hands = @step.hands.split(",")
-    @damage_points = @step.damage_points.split(",").map(&:to_i)
-    @remaining_life_points = @step.remaining_life_points.split(",").map(&:to_i)
+    @step = steps[params[:id].to_i - 1].decorate
     @is_last_step = steps.size == params[:id].to_i
     return unless @is_last_step
-
-    @winner = @fight.winner
-    @winner = @fight.loser
   end
 end
