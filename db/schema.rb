c56d5f36ad41cc158379d515869154218b6af8dc
Matthew Nguyen <contact@n-studio.fr>
2022-01-16 15:53:19 +0800

    chore: update dependencies

--- db/schema.rb
+++ db/schema.rb
@@ -2,8 +2,8 @@
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
 #
-# This file is the source Rails uses to define your schema when running `rails
-# db:schema:load`. When creating a new database, `rails db:schema:load` tends to
+# This file is the source Rails uses to define your schema when running `bin/rails
+# db:schema:load`. When creating a new database, `bin/rails db:schema:load` tends to
 # be faster and is potentially less error prone than running all of your
 # migrations from scratch. Old migrations may fail to apply correctly if those
 # migrations use external dependencies or application code.
@@ -13,8 +13,8 @@
 ActiveRecord::Schema.define(version: 2019_09_09_001522) do
 
   create_table "fight_attendances", force: :cascade do |t|
-    t.integer "fight_id"
-    t.integer "fighter_id"
+    t.bigint "fight_id"
+    t.bigint "fighter_id"
     t.boolean "winner", default: false
     t.boolean "loser", default: false
     t.datetime "created_at", precision: 6, null: false
@@ -24,7 +24,7 @@ ActiveRecord::Schema.define(version: 2019_09_09_001522) do
   end
 
   create_table "fight_steps", force: :cascade do |t|
-    t.integer "fight_id", null: false
+    t.bigint "fight_id", null: false
     t.string "hands", null: false
     t.string "damage_points", default: "0,0"
     t.datetime "created_at", precision: 6, null: false
